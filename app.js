a0ccedf000bd0d2619f293130d2a277793ad326d
AsadAyub88 <125857371+AsadAyub88@users.noreply.github.com>
2023-11-15 01:22:10 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,39 +3,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const consentButton = document.getElementById('consentButton');
     const galleryContainer = document.querySelector('.gallery-container');
-
+    const fileInput = document.getElementById('fileInput');
+    
     consentButton.addEventListener('click', () => {
-        // Check for browser compatibility
-        if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-            // Request access to the user's media (gallery)
-            navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-                .then(handleMediaStream)
-                .catch(handleError);
-        } else {
-            alert('Media device access is not supported in this browser.');
-        }
+        fileInput.click();
     });
 
-    function handleMediaStream(stream) {
-        const videoTrack = stream.getVideoTracks()[0];
+    fileInput.addEventListener('change', handleFileSelection);
 
-        // Access the last image from the video track
-        const imageCapture = new ImageCapture(videoTrack);
-        imageCapture.grabFrame()
-            .then(displayLastImage)
-            .catch(handleError);
+    function handleFileSelection(event) {
+        const selectedFile = event.target.files[0];
+
+        if (selectedFile) {
+            displayLastImage(selectedFile);
+        } else {
+            alert('No file selected. Please try again.');
+        }
     }
 
-    function displayLastImage(imageBitmap) {
+    function displayLastImage(imageFile) {
         const lastImage = document.getElementById('lastImage');
-        lastImage.src = URL.createObjectURL(imageBitmap);
+        lastImage.src = URL.createObjectURL(imageFile);
 
         // Show the gallery container
         galleryContainer.style.display = 'block';
-    }
-
-    function handleError(error) {
-        console.error('Error accessing gallery:', error);
-        alert('There was an error accessing your gallery. Please try again.');
     }
 });
