064a598afa8be7929a957d51ecbdc1ea4000cbfb
AsadAyub88 <125857371+AsadAyub88@users.noreply.github.com>
2023-11-14 23:40:23 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,34 @@ function handleFile(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
 
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         const imageData = e.target.result;
-        // Use a barcode scanning library here to process imageData
-        // Update the result div with the scanned barcode or product name
+
+        // Use QuaggaJS library for barcode decoding
+        decodeBarcode(imageData);
     };
 
     reader.readAsDataURL(file);
+}
+
+function decodeBarcode(imageData) {
+    Quagga.decodeSingle({
+        src: imageData,
+        numOfWorkers: 0,  // Setting to 0 to run on the main thread (no web workers)
+        inputStream: {
+            size: 800  // Adjust this size based on your needs
+        },
+        decoder: {
+            readers: ['ean_reader']  // Use 'ean_reader' for EAN-13 barcodes
+            // Add more reader types based on the types of barcodes you expect
+        },
+    }, function (result) {
+        if (result && result.codeResult) {
+            // Update the result div with the decoded barcode
+            document.getElementById('result').innerText = `Barcode: ${result.codeResult.code}`;
+        } else {
+            // Handle case when barcode is not detected
+            document.getElementById('result').innerText = 'Barcode not detected.';
+        }
+    });
 }
